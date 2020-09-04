1d28be870adf9d671024a869b44e3fa11104a3c5
sandrahy <sandra.hernandez.y@usach.cl>
2020-09-04 19:35:49 -0400

    saving better goals

--- ILS.R
+++ ILS.R
@@ -1,6 +1,10 @@
 library(ggplot2)
 library(ggmap)
 
+time_instances <-list()             # Tiempo que tarda la ejecucion de una instancia
+inst_best_objetive <-list()         # Mejores objetivos por instancia
+inst_best_objetive_iter <-list() # Mejores objetivos por iteracion
+
 objectiveFunction <- function(sismos, centroides){
   totalDist = 0
   for (i in 1:length(sismos)) {
@@ -16,29 +20,42 @@ objectiveFunction <- function(sismos, centroides){
 
 
 ILS <- function(max_iteraciones,funcion_vecindad,sismos, max_clusters,medians){
-  #' Inicializando soluciones inicial
+  # Inicializando soluciones inicial
   centroids <- initialSolution(sismos, num_centroids=max_clusters)
   lista <- kmedians(sismos,centroids,medians)
   sismos <- lista[[1]]
   centroids <- lista[[2]]           
   dist <- objectiveFunction(sismos,centroids)
   
+  bestObjective= dist
+  objectives <- list()
+  objectives <- append(objectives,dist)  
+  
   for (i in 1:max_iteraciones) {
+    
     if(funcion_vecindad == 1){
       centroids_new = randomSwap(centroids, sismos)
     }else{
       centroids_new = averageNeighbour(centroids, sismos)
     }
+    
     sismos_new <- sismos
     lista_new <- kmedians(sismos_new, centroids_new,medians)
     sismos_new <- lista_new[[1]]
     centroids_new = lista_new[[2]]
     dist_new <- objectiveFunction(sismos_new,centroids_new)
+    
     if (dist_new < dist){
       sismos <- sismos_new
       centroids <- centroids_new
+      bestObjective= dist
+      objectives <- append(objectives,dist)
     }
   }
+  
+  inst_best_objetive_iter<<- append(inst_best_objetive_iter, objectives)
+  inst_best_objetive<<- append(inst_best_objetive, bestObjective)
+  
   final  <- list(sismos,centroids)
   return (final)
 }
@@ -58,7 +75,19 @@ centroides_finales <- finales[[2]]
 
 
 
-###--------------Plotting
+
+
+
+
+
+
+
+
+
+
+
+
+###--------------Plotting------------------
 long <- list()
 lat <- list()
 cluster <- list()
