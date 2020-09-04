6eb79aa5ada3d56d8083be1f160683ebd227256b
sandrahy <sandra.hernandez.y@usach.cl>
2020-09-04 02:25:51 -0400

    k-medians

--- file_functions.R
+++ file_functions.R
@@ -1,10 +1,3 @@
-#install.packages('readr')
-#library(readr)
-
-#Option 1
-#file.choose(): pantalla emergente para selecionar archivo "Sismos_Ecuador"
-# puede ser remplazado por la ruta del archivo "Sismos_Ecuador"
-
 #data <- read.csv(file.choose(),skip = 0)
 
 #data
@@ -14,6 +7,7 @@
 #length(data) retorna la cantidad de columnas
 #nrow(data)   retorna la cantidad de filas
 #data[[1]]    retorma TODA la columna
+#------------------------------------------------------
 #sismos[i]          Acceso a cada sismo i con todas sus caracteristicas
 #sismos[[i]][[j]]   Acceso a la elem j del sismo i
 #length(sismos)     retorna cantidad de filas 
@@ -22,7 +16,6 @@
 #Option 2
 tusDatos <- read.table(file.choose(), skip = 0, header = TRUE, sep =',')
 
-
 # Initialize `sismos`
 sismos <- list()
 # Creando y llenando la estructura de datos listas de listas `sismos`
@@ -30,3 +23,16 @@ for(i in 1:nrow(tusDatos)) {
   sismos[[i]] <- list(tusDatos[i,1],tusDatos[i,2],tusDatos[i,3],tusDatos[i,4],tusDatos[i,5],tusDatos[i,6],tusDatos[i,7],tusDatos[i,8],tusDatos[i,9])
 }
 
+#PRUEBA: centrides <- initialSolution(sismos, num_centroids=3)
+#RESPUESTA: [[1]] 
+#           [[1]][[1]] [1] 1
+#           [[1]][[2]] [1] 3.402023
+#           [[1]][[3]] [1] 0.88
+#           [[2]]
+#           [[2]][[1]] [1] 2
+#           [[2]][[2]] [1] 10
+#           [[2]][[3]] [1] 0.22
+#           [[3]]
+#           [[3]][[1]] [1] 3
+#           [[3]][[2]] [1] 0.4745412
+#           [[3]][[3]] [1] -0.25
