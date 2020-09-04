c004db3b96e10df6f1fdf60b7b0900b3b554ca12
shali7296 <shalini.ramchandani@usach.cl>
2020-09-04 13:51:37 -0400

    Plotting

--- ILS.R
+++ ILS.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 objectiveFunction <- function(sismos, centroides){
   totalDist = 0
   for (i in 1:length(sismos)) {
@@ -34,24 +36,43 @@ ILS <- function(max_iteraciones,funcion_vecindad,sismos,centroids){
   return (final)
 }
 
-centroids <- initialSolution(sismos, num_centroids=5)
+centroids <- initialSolution(sismos, num_centroids=3)
 lista <- kmedians(sismos,centroids)
-print(lista)
+
 sismos <- lista[[1]]
 centroids <- lista[[2]]
-finales <- ILS(3, 1,sismos,centroids)
+finales <- ILS(50, 1,sismos,centroids)
 sismos_finales <- finales[[1]]
 centroides_finales <- finales[[2]]
 
 
 ###Plotting
-#long <- list()
-#lat <- list()
-#cluster <- list()
-#for (i in 1:lenght(sismos)) {
-#  lat <- append(lat, sismos_finales[[i]][[2]])
-#  long <- append(long, sismos_finales[[i]][[3]])
-#  cluster <- append(cluster, sismos_finales[[i]][[9]])
-#}
+long <- list()
+lat <- list()
+cluster <- list()
+for (i in 1:length(sismos)) {
+  lat <- append(lat, sismos_finales[[i]][[2]])
+  long <- append(long, sismos_finales[[i]][[3]])
+  cluster <- append(cluster, sismos_finales[[i]][[9]])
+}
+
+#hola <- list(1,2,3,4,5)
+#chao <- list(6,7,8,9,10)
+#juju <- c(hola,chao)
+#matriz <- matrix(juju, nrow = 2, dimnames = list(c(),c("Col1","Col2","Col1","Col2","Col5")))
+
+x <- matrix(c(lat,long,cluster), nrow = length(sismos), dimnames = list(c(),c("Lat","Long","Cluster")))
+x_new <- as.data.frame(x)
+x_new$Lat = as.numeric(x_new$Lat)
+x_new$Long = as.numeric(x_new$Long)
+x_new$Cluster = as.numeric(x_new$Cluster)
+x_new$Cluster = as.factor(x_new$Cluster)
+#ggplot(data = x_new, aes(Lat, Long))
+#+ 
+ # geom_point(aes(Lat, Long, colour = factor(Cluster)))
+
+ggplot(x_new, aes(x = Long, y = Lat, colour = Cluster)) + geom_point()
+
+
 
 #plot(x = sismos_finales[[i]][[2]], y = sismos_finales[[i]][[3]])
